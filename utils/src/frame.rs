b891301e76f21a95a232237bfe7dca10ba1aa866
etiennecollin <collin.etienne.contact@gmail.com>
2024-11-08 18:08:33 -0500

    Implemented Frame functions

--- utils/src/frame.rs
+++ utils/src/frame.rs
@@ -1,6 +1,5 @@
 #[derive(Debug)]
 pub struct Frame {
-    pub flag: u8,
     pub frame_type: u8,
     pub num: u8,
     pub data: Vec<u8>,
@@ -8,13 +7,31 @@ pub struct Frame {
 }
 
 impl Frame {
+    pub const FLAG: u8 = 0x7E;
+
     pub fn new(frame_type: u8, num: u8, data: Vec<u8>, crc: u16) -> Frame {
         Frame {
-            flag: 0x7E, // 0111 1110
             frame_type,
             num,
             data,
             crc,
         }
+    }
+
+    pub fn to_bytes(&self) -> Vec<u8> {
+        let mut bytes = vec![Frame::FLAG, self.frame_type, self.num];
+        bytes.append(&mut self.data.clone());
+        bytes.append(&mut self.crc.to_be_bytes().to_vec());
+        bytes.push(Frame::FLAG);
+        bytes
+    }
+
+    pub fn from_bytes(bytes: Vec<u8>) -> Frame {
+        let frame_type = bytes[1];
+        let num = bytes[2];
+        let data = bytes[3..bytes.len() - 3].to_vec();
+        let crc = u16::from_be_bytes([bytes[bytes.len() - 3], bytes[bytes.len() - 2]]);
+
+        Frame::new(frame_type, num, data, crc)
     }
 }

526a21574c69c1e300dc84325d801af054e0f052
etiennecollin <collin.etienne.contact@gmail.com>
2024-11-12 23:22:21 -0500

    Set proper doc URL

--- utils/src/crc.rs
+++ utils/src/crc.rs
@@ -12,7 +12,7 @@ const LUT: [PolynomialSize; 256] = lookup_table();
 /// remainder of the polynomial for the corresponding byte value.
 ///
 /// The implementation was inspired by:
-/// - https://barrgroup.com/blog/crc-series-part-3-crc-implementation-code-cc
+/// - <https://barrgroup.com/blog/crc-series-part-3-crc-implementation-code-cc>
 const fn lookup_table() -> [PolynomialSize; 256] {
     let mut table = [0; 256];
     let mut input: usize = 0;
